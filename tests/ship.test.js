8d18198d166d25f0475d293df54ead9382d2ef65
Arjun Saili <arjun7.saili@gmail.com>
2021-11-27 11:24:53 -0500

    Fixed Errors in Testing Gameboard

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -57,13 +57,15 @@ test("Check if attack is received (hit)", ()=>{
     gameboard.placeShip([0,0], 5, "vertical");
     gameboard.receiveAttack([0,0]);
     expect(gameboard.getShips()[0].ship.getShipArray()[0].isHit).toBe(true);
+    expect(gameboard.getMissedShots()).toStrictEqual([]);
 })
 
 test("Check if attack is received (miss)", ()=>{
     const gameboard = Gameboard();
     gameboard.placeShip([0,0], 5, "horizontal");
-    gameboard.receiveAttack([4,0])
-    expect(gameboard.getShips()[0].ship.getShipArray()[4].isHit).toBe(true)
+    gameboard.receiveAttack([4,7])
+    expect(gameboard.getShips()[0].ship.getShipArray()[4].isHit).toBe(false)
+    expect(gameboard.getMissedShots()).toStrictEqual([[4,7]])
 })
 
 test("Hit till sunk", ()=>{
@@ -74,7 +76,8 @@ test("Hit till sunk", ()=>{
     gameboard.receiveAttack([4,5]);
     gameboard.receiveAttack([5,5]);
     gameboard.receiveAttack([6,5]);
-    console.log(gameboard.getShips()[0].coords);
     expect(gameboard.getShips()[0].ship.isSunk()).toBe(true)
     expect(gameboard.allSunk()).toBe(true)
+    expect(gameboard.getMissedShots()).toStrictEqual([]);
 })
+
