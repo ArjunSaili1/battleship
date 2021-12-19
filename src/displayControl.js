89167cbc47119da908b24405c0ee856d967619ad
Arjun Saili <arjun7.saili@gmail.com>
2021-12-19 09:12:49 -0500

    Added Gameboard rendering in DOM

--- src/displayControl.js
+++ src/displayControl.js
@@ -1,14 +1,44 @@
 const displayControl = (()=>{
     
+    const grids = document.querySelectorAll(".grid-container");
+
     function render(playerGameboard, computerGameboard){
         generateGrids();
+        renderShips(computerGameboard.getShips(), grids[1]);
+        renderShips(playerGameboard.getShips(), grids[0]);
+
     }
 
+    function renderShips(gameboardShips, gameboardGrid){
+        let coordColour;
+        for(let i=0; i < gameboardShips.length; i++){
+            console.log(gameboardShips[i].coords.length);
+            if(gameboardShips[i].ship.isSunk()){
+                coordColour = 'black';
+            }
+            for(let k = 0; k < gameboardShips[i].ship.getShipArray().length; k++){
+                if(gameboardShips[i].ship.getShipArray()[k].isHit && !(gameboardShips[i].ship.isSunk())){
+                    coordColour = 'red';
+                }
+                else if(!gameboardShips[i].ship.isSunk()){
+                    coordColour = 'blue';
+                };
+                for(let m = 0; m < gameboardGrid.children.length; m++){
+                    if(gameboardGrid.children[m].dataset.xCoordinate == gameboardShips[i].coords[k][0] &&
+                        gameboardGrid.children[m].dataset.yCoordinate == gameboardShips[i].coords[k][1]){
+                            gameboardGrid.children[m].style.backgroundColor = coordColour;
+                        };
+                }
+            }
+        }
+    }
+
+    
+
     function generateGrids(){
-        let xCoord = 0;
-        let yCoord = 0;
-        const grids = document.querySelectorAll(".grid-container");
         for(let i = 0; i < grids.length; i++){
+            let xCoord = 0;
+            let yCoord = 0;
             for(let j = 0; j < 100; j++){
                 const newCoord = document.createElement("div");
                 newCoord.classList.add("coordinate");
--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import { displayControl } from './displayControl.js';
 const game = (()=>{
 
     function gameSetup(){
-        displayControl.render();
         const computerPlayer = Computer();
         computerPlayer.placeAllShips();
         const player = Player("Test Name");
         player.gameboard.placeShip([0, 0], 5, "horizontal");
-        player.gameboard.placeShip([1, 0], 5, "horizontal");
-        player.gameboard.placeShip([2, 0], 5, "horizontal");
-        player.gameboard.placeShip([3, 0], 5, "horizontal");
-        player.gameboard.placeShip([4, 0], 5, "horizontal");
+        player.gameboard.placeShip([0, 2], 4, "horizontal");
+        player.gameboard.placeShip([0, 4], 3, "horizontal");
+        player.gameboard.placeShip([0, 6], 3, "horizontal");
+        player.gameboard.placeShip([0, 8], 2, "horizontal");
+        player.gameboard.receiveAttack([0,0]);
+        displayControl.render(player.gameboard, computerPlayer.gameboard);
     }
 
     return {gameSetup}
